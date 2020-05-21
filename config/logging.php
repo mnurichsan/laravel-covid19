ac05db457fed8215432b3b1e69929faabd3562bb
muhnurichsan <iccankblog@gmail.com>
2020-05-21 22:12:31 +0700

    use errorlog

--- config/logging.php
+++ config/logging.php
@@ -38,12 +38,11 @@ return [
         'stack' => [
             'driver' => 'stack',
             'channels' => ['single'],
-            'ignore_exceptions' => false,
+            
         ],
 
         'single' => [
             'driver' => 'single',
-            'path' => storage_path('logs/laravel.log'),
             'level' => 'debug',
         ],
 
